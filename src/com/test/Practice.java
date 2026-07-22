5df7e5ac95c435ec715cb151a83bfe05effc32a7
PremSagar <premsagarbandi.dev@gmail.com>
2026-07-22 22:44:59 +0530

    For conflict

--- src/com/test/Practice.java
+++ src/com/test/Practice.java
@@ -12,7 +12,7 @@ public class Practice {
 		int b=11;
 		int c=a+b;
 		System.out.println("Sum of a And b is c : ", c);
-		Sysem.out.println("New line added")
+		Sysem.out.println("This is for conflict")
 
 	}
 }
