33913d569ff03f4d893d51686202f82fc2a9ff46
PremSagar <premsagarbandi.dev@gmail.com>
2026-07-24 13:38:44 +0530

    feature: added a new display method

--- src/com/test/Practice.java
+++ src/com/test/Practice.java
@@ -8,6 +8,10 @@ public class Practice {
 		System.out.println("Hello Fouzia!");
 		System.out.println("Welcome to GitHub");
 		Sysem.out.println("This is for conflict");
+		
+	}
 
+	public void display(){
+		System.out.println("New branch added");
 	}
 }
